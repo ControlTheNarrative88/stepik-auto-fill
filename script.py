4e140c5aec8f595353581a7c6624c1e22699cc93
unknown <andri.elezov@gmail.com>
2022-09-17 02:37:47 +0300

    script commit

---
+++ script.py
@@ -0,0 +1,57 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+email = "test.user.05@inbox.ru"
+password = "testpassword0505"
+stepik_url = "https://stepik.org/catalog"
+task_link = "https://stepik.org/lesson/236205/step/7?auth=registration&unit=208637"
+answer = "text_to_be_present_in_element"
+
+browser = webdriver.Chrome()
+browser.get(stepik_url)
+
+browser.implicitly_wait(10)
+
+button_login = browser.find_element(By.ID, "ember246").click()
+
+input_email = browser.find_element(By.ID, "id_login_email").send_keys(email)
+
+input_password = browser.find_element(By.ID, "id_login_password").send_keys(password)
+
+button_voity = browser.find_element(By.CSS_SELECTOR, "button.sign-form__btn.button_with-loader").click()
+
+time.sleep(3)
+
+browser.get(task_link)
+
+def check_exists():
+    try:
+        browser.find_element(By.CSS_SELECTOR, "button.again-btn.white")
+    except NoSuchElementException:
+        return False
+    return True
+
+def send_answer():
+    input_answer = browser.find_element(By.CSS_SELECTOR,
+                                        "textarea.ember-text-area.ember-view.textarea.string-quiz__textarea")
+    input_answer.send_keys(answer)
+    button_submit = browser.find_element(By.CSS_SELECTOR, "button.submit-submission")
+    button_submit.click()
+
+
+if browser.current_url == task_link:
+    if check_exists():
+        button_again = browser.find_element(By.CSS_SELECTOR, "button.again-btn.white")
+        button_again.click()
+        time.sleep(2)
+        send_answer()
+    else:
+        send_answer()
+
+else:
+    if check_exists():
+        button_unit = browser.find_element(By.ID, "ember936")
+        button_unit.click()
+        send_answer()
