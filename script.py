dd5931ba74e1eda9a10f0702a72a079e22013380
unknown <andri.elezov@gmail.com>
2022-09-17 03:06:33 +0300

    requirements, modded script

--- script.py
+++ script.py
@@ -3,37 +3,35 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-email = "test.user.05@inbox.ru"
+email = "test.user.05@inbox.ru"                                                       # тестовый юзер, могут быть любые валидные данные
+                                                                                      # надо быть записанным на курс https://stepik.org/course/575/syllabus?auth=registration
 password = "testpassword0505"
 stepik_url = "https://stepik.org/catalog"
-task_link = "https://stepik.org/lesson/236205/step/7?auth=registration&unit=208637"
-answer = "text_to_be_present_in_element"
+task_link = "https://stepik.org/lesson/236205/step/7?auth=registration&unit=208637"   # валидная сылка на задание
+answer = "text_to_be_present_in_element"                                              # любая str, но лучше с правильным ответом))
 
 browser = webdriver.Chrome()
 browser.get(stepik_url)
 
 browser.implicitly_wait(10)
 
-button_login = browser.find_element(By.ID, "ember246").click()
-
+button_login = browser.find_element(By.ID, "ember246").click()                        # авторизация и открытие задания
 input_email = browser.find_element(By.ID, "id_login_email").send_keys(email)
-
 input_password = browser.find_element(By.ID, "id_login_password").send_keys(password)
-
 button_voity = browser.find_element(By.CSS_SELECTOR, "button.sign-form__btn.button_with-loader").click()
 
 time.sleep(3)
 
 browser.get(task_link)
 
-def check_exists():
-    try:
+def check_exists():                                                                   # функция проверяет наличие кнопки "решить снова
+    try:                                                                              # т.е. было ли задание решено до этого
         browser.find_element(By.CSS_SELECTOR, "button.again-btn.white")
     except NoSuchElementException:
         return False
     return True
 
-def send_answer():
+def send_answer():                                                                    # отправка ответа на степик
     input_answer = browser.find_element(By.CSS_SELECTOR,
                                         "textarea.ember-text-area.ember-view.textarea.string-quiz__textarea")
     input_answer.send_keys(answer)
